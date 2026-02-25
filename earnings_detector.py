114fbb3920e4fd9f029ff43f001fae8c94c4d38f
theblogspirit <theblogspirit@gmail.com>
2026-02-25 22:59:57 +0530

    Update earnings_detector.py

--- earnings_detector.py
+++ earnings_detector.py
@@ -1,11 +1,10 @@
 import requests
 
-URL = "https://www.nseindia.com/api/corporates-financial-results"
+URL = "https://www.nseindia.com/api/corporates-financial-results?index=equities"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.nseindia.com/"
 }
 
